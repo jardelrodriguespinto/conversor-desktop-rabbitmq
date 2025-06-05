c5493a9dac3e34d22ad70fba2dc8dd80bbd5062d
jardelrodriguespinto <ghmagno910@gmail.com>
2025-06-05 00:04:48 -0300

    logica para verificar arquivo

--- conversor.py
+++ conversor.py
@@ -2,22 +2,53 @@ from tkinter import *
 from tkinter import ttk
 import os
 from tkinter import filedialog
+from enum import Enum
+
 
 # file = open("arquivo.txt", encoding="utf8")
 #
 # file.read()
 
+class FileExtention(Enum):
+    PDF = ".pdf"
+    JPG = ".jpg"
+    PNG = ".png"
+    BMP = ".bmp"
+    WEBP = ".webp"
+    TXT = ".txt"
+
 def abrir_explorer():
     file = filedialog.askopenfilename()
 
-root = Tk()
+    if any(file.endswith(ext.value) for ext in FileExtention):
+        print(file)
+    else:
+        print("Formato não suportado")
 
+
+root = Tk()
 frm = ttk.Frame(root, padding=100)
 
 frm.grid()
 
-ttk.Label(frm, text="Abrir Arquivo").grid(column=1, row=0)
 ttk.Progressbar().location(10, 10)
+
+
+monthchoosen = ttk.Combobox(frm, width = 270, textvariable = "asd")
+
+# Adding combobox drop down list
+monthchoosen['values'] = (' January',
+                          ' February',
+                          ' March',
+                          ' April',
+                          ' May',
+                          ' June',
+                          ' July',
+                          ' August',
+                          ' September',
+                          ' October',
+                          ' November',
+                          ' December')
 
 button = ttk.Button(frm, text="Carregar arquivo", command=abrir_explorer).grid(column=1, row=1)
 
