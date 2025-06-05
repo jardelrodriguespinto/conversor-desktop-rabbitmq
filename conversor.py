3470434bc064a341b83c5ba2de23d7e8230322df
jardelrodriguespinto <ghmagno910@gmail.com>
2025-06-04 23:24:21 -0300

    iniciando a tela

--- conversor.py
+++ conversor.py
@@ -1,15 +1,28 @@
 from tkinter import *
 from tkinter import ttk
+import os
+from tkinter import filedialog
 
 # file = open("arquivo.txt", encoding="utf8")
 #
 # file.read()
 
+def abrir_explorer():
+    file = filedialog.askopenfilename()
 
 root = Tk()
-frm = ttk.Frame(root, padding=10)
+
+frm = ttk.Frame(root, padding=100)
+
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Progressbar()
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+ttk.Label(frm, text="Abrir Arquivo").grid(column=1, row=0)
+ttk.Progressbar().location(10, 10)
+
+button = ttk.Button(frm, text="Carregar arquivo", command=abrir_explorer).grid(column=1, row=1)
+
+
+
+
 root.mainloop()
+
